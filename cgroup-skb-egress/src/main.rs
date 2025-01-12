765f2239da850b9c04d313166003f20b7104e1b8
shishir9159 <nahidhasan43@iut-dhaka.edu>
2025-01-12 08:57:06 +0600

    listing containers, but it is a wrong design choice. Listing should be done with containerd or cri-api by namespace

--- cgroup-skb-egress/src/main.rs
+++ cgroup-skb-egress/src/main.rs
@@ -1,4 +1,11 @@
 use std::net::Ipv4Addr;
+use k8s_openapi::api::core::v1::Pod;
+use kube::{
+    api::{Api, ApiResource, NotUsed, Object, ResourceExt},
+    Client,
+};
+use serde::Deserialize;
+use tracing::*;
 
 use aya::{
     maps::{perf::AsyncPerfEventArray, HashMap},
@@ -19,6 +26,52 @@ struct Opt {
 //     todo: include memory QOS for next k8s update
    #[clap(short, long, default_value = "/sys/fs/cgroup/kubepods.slice/kubepods-burstable.slice/")]
     cgroup_path: String,
+}
+
+#[tokio::main]
+async fn listing() -> anyhow::Result<()> {
+    tracing_subscriber::fmt::init();
+    let client = Client::try_default().await?;
+
+    // Here we replace heavy type k8s_openapi::api::core::v1::PodSpec with
+    #[derive(Clone, Deserialize, Debug)]
+    struct PodSpecSimple {
+        containers: Vec<ContainerSimple>,
+    }
+    #[derive(Clone, Deserialize, Debug)]
+    struct ContainerSimple {
+        #[allow(dead_code)]
+        image: String,
+    }
+    type PodSimple = Object<PodSpecSimple, NotUsed>;
+
+    // Here we simply steal the type info from k8s_openapi, but we could create this from scratch.
+    let ar = ApiResource::erase::<k8s_openapi::api::core::v1::Pod>(&());
+
+    let pods: Api<PodSimple> = Api::default_namespaced_with(client, &ar);
+    for p in pods.list(&Default::default()).await? {
+        info!("Pod {} runs: {:?}", p.name_any(), p.spec.containers);
+    }
+
+    Ok(())
+}
+
+fn containersID() -> anyhow::Result<()> {
+
+    let pods: Api<Pod> = Api::default_namespaced(client);
+
+    let pod = pods.get("blog").await?;
+    println!("Got pod: {pod:?}");
+
+    let patch = json!({"spec": {
+    "activeDeadlineSeconds": 5
+}});
+    let pp = PatchParams::apply("kube");
+    let patched = pods.patch("blog", &pp, &Patch::Apply(patch)).await?;
+    assert_eq!(patched.spec.active_deadline_seconds, Some(5));
+
+    pods.delete("blog", &DeleteParams::default()).await?;
+
 }
 
 #[tokio::main]
